e2d9582859c2763fb65722202d5964cc0c155b9d
Samuel Kimote <readssamuel@.com>
2023-09-29 16:25:34 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+  
+  function superbowlWin(record) {
+    const winningYearEntry = record.find(entry => entry.result === 'W' && entry.year === '2015');
+  
+    return winningYearEntry ? winningYearEntry.year : undefined; 
+  }
+  
+  const winningYear = superbowlWin(record);
+  
+  console.log(winningYear); 
+  
